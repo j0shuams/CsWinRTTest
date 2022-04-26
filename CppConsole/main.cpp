77b53a513aa9307bd46c681adae6023b4a6c1c5b
Joshua Larkin <70237359+j0shuams@users.noreply.github.com>
2022-04-26 11:38:18 -0700

    init

---
+++ CppConsole/main.cpp
@@ -0,0 +1,15 @@
+﻿#include "pch.h"
+
+using namespace winrt;
+using namespace Windows::Foundation;
+using namespace Coords;
+
+int main()
+{
+    init_apartment();
+    Uri uri(L"http://aka.ms/cppwinrt");
+    printf("Hello, %ls!\n", uri.AbsoluteUri().c_str());
+    Coord a = Coord();
+    Coord b = Coord(3, 4);
+    printf("Distance between %ls and %ls is %f\n", a.ToString().c_str(), b.ToString().c_str(), a.Distance(b));
+}
---
+++ Native/Native/MainWindow.xaml.cpp
@@ -0,0 +1,42 @@
+﻿#include "pch.h"
+#include "MainWindow.xaml.h"
+#if __has_include("MainWindow.g.cpp")
+#include "MainWindow.g.cpp"
+#endif
+
+using namespace winrt;
+using namespace Microsoft::UI::Xaml;
+using namespace winrt::Coords;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace winrt::Native::implementation
+{
+    MainWindow::MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    int32_t MainWindow::MyProperty()
+    {
+        throw hresult_not_implemented();
+    }
+
+    void MainWindow::MyProperty(int32_t /* value */)
+    {
+        throw hresult_not_implemented();
+    }
+
+    void MainWindow::myButton_Click(IInspectable const&, RoutedEventArgs const&)
+    {
+        Coord a = Coord();
+        Coord b = Coord(3, 4);
+        if (a.Distance(b) == 5) {
+            myButton().Content(box_value(L"Clicked"));
+        }
+        else {
+            myButton().Content(box_value(L"Not five?!"));
+        }
+    }
+}
